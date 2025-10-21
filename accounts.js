c573059df5b8d6c21d529b66ab8f4a1d102bdb86
COL-OUR <96907856+COL-OUR@users.noreply.github.com>
2025-10-21 21:13:47 +1100

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,24 +1,24 @@
 // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-   firebaseConfig = {
-    apiKey: "AIzaSyDN6J8OMON2LWsQy52yUDISQehDMTsk1bw",
-    authDomain: "futurestar-08.firebaseapp.com",
-    projectId: "futurestar-08",
-    storageBucket: "futurestar-08.firebasestorage.app",
-    messagingSenderId: "465929730039",
-    appId: "1:465929730039:web:54a6d51a3a8fb6ccb720f2",
-    measurementId: "G-MDX2WDZV0Y"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDN6J8OMON2LWsQy52yUDISQehDMTsk1bw",
+  authDomain: "futurestar-08.firebaseapp.com",
+  projectId: "futurestar-08",
+  storageBucket: "futurestar-08.firebasestorage.app",
+  messagingSenderId: "465929730039",
+  appId: "1:465929730039:web:54a6d51a3a8fb6ccb720f2",
+  measurementId: "G-MDX2WDZV0Y"
+};
 
-  // Initialize Firebase
-   app = initializeApp(firebaseConfig);
-   analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
