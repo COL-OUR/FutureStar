e65f016e91ed342b3d37d901b85bed7f32fa4df6
COL-OUR <96907856+COL-OUR@users.noreply.github.com>
2025-10-21 22:40:03 +1100

    Update Accounts_dashboard.js

--- accounts/Accounts_dashboard.js
+++ accounts/Accounts_dashboard.js
@@ -1,11 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js"
 
+// Firebase configuration
 const firebaseConfig = {
-    //YOUR COPIED FIREBASE PART SHOULD BE HERE
- //WATCH THIS VIDEO TO LEARN WHAT TO PUT HERE   https://youtu.be/_Xczf06n6x0
-  };
+  apiKey: "AIzaSyDN6J8OMON2LWsQy52yUDISQehDMTsk1bw",
+  authDomain: "futurestar-08.firebaseapp.com",
+  projectId: "futurestar-08",
+  storageBucket: "futurestar-08.firebasestorage.app",
+  messagingSenderId: "465929730039",
+  appId: "1:465929730039:web:54a6d51a3a8fb6ccb720f2",
+  measurementId: "G-MDX2WDZV0Y"
+};
  
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
